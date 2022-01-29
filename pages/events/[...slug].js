5b80be15e791de3abe7072dfc52f77cb6239a03d
NamTran <nama3k67lnq@gmail.com>
2022-01-30 00:20:48 +0700

    first-commit

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -5,21 +5,21 @@ import EventList from "../../components/events/event-list";
 import ErrorAlert from "../../components/ui/error-alert";
 import Button from "../../components/ui/button";
 
-import { getFilteredEvents } from "../../dummy-data";
+import { getFilteredEvents } from "../../helpers/api-util";
 
-const FilteredEventsPage = ({ hasError, filterData }) => {
+const FilteredEventsPage = ({ hasError, filteredEvents, date }) => {
   // const router = useRouter();
   // const filterData = router.query.slug;
 
-  if (!filterData) {
-    return <p className="center">Loading...</p>;
-  }
+  // if (!filterData) {
+  //   return <p className="center">Loading...</p>;
+  // }
 
-  const filteredYear = filterData[0];
-  const filteredMonth = filterData[1];
+  // const filteredYear = filterData[0];
+  // const filteredMonth = filterData[1];
 
-  const numYear = +filteredYear;
-  const numMonth = +filteredMonth;
+  // const numYear = +filteredYear;
+  // const numMonth = +filteredMonth;
 
   if (hasError) {
     return (
@@ -34,11 +34,6 @@ const FilteredEventsPage = ({ hasError, filterData }) => {
     );
   }
 
-  const filteredEvents = getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  });
-
   if (!filteredEvents || filteredEvents.length === 0) {
     return (
       <>
@@ -52,11 +47,11 @@ const FilteredEventsPage = ({ hasError, filterData }) => {
     );
   }
 
-  const date = new Date(numYear, numMonth - 1);
+  const resultDate = new Date(date.year, date.month - 1);
 
   return (
     <>
-      <ResultTitle date={date} />
+      <ResultTitle date={resultDate} />
       <EventList items={filteredEvents} />
     </>
   );
@@ -87,6 +82,21 @@ export const getServerSideProps = async ({ params }) => {
       // }
     };
   }
+
+  const filteredEvents = await getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  return {
+    props: {
+      filteredEvents,
+      date: {
+        year: numYear,
+        month: numMonth,
+      },
+    },
+  };
 };
 
 export default FilteredEventsPage;
