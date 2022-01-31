d4c54f2ad9ad700537a55c5b8510b302c03b6d14
NamTran <nama3k67lnq@gmail.com>
2022-01-31 15:22:24 +0700

    use Image optimization

--- components/event-detail/event-logistics.js
+++ components/event-detail/event-logistics.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import DateIcon from "../icons/date-icon";
 import AddressIcon from "../icons/address-icon";
 import LogisticsItem from "./logistics-item";
@@ -15,7 +17,7 @@ const EventLogistics = ({ date, address, image, imageAlt }) => {
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={"/" + image} alt={imageAlt} />
+        <Image src={"/" + image} alt={imageAlt} width={400} height={400} />
       </div>
 
       <ul className={classes.list}>
--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import DateIcon from "../icons/date-icon";
 import AddressIcon from "../icons/address-icon";
 import ArrowRightIcon from "../icons/arrow-right-icon";
@@ -17,7 +19,7 @@ const EventItem = ({ title, image, date, location, id }) => {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      <Image src={"/" + image} alt={title} width={250} height={160} />
 
       <div className={classes.content}>
         <div className={classes.summary}>
